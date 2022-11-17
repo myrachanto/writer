e0a54814ff1deaee7aef0eeda4b5391c98fec3ce
myrachanto <myrachanto@gmail.com>
2022-11-17 14:43:06 +0300

    special characters

--- components/patches/section1.js
+++ components/patches/section1.js
@@ -11,7 +11,7 @@ const Section1 = () => {
       <h2 className="text-lg font-bold">Thesis assignment help</h2>
        <p className="text-base peer">
        Your thesis may also be considered by different employers when processing your job application in the future. The high-quality of our completed dissertation examples have had a positive impact on our customer base over the years.
-      Research papers assignment will be considered by your instructors when determining the final grade at the end of your studies. Hence, you should seek research writing to ensure that you submit a quality college paper that will guarantee good grades in the end. When you search for a research paper writing service cheap, you will realize that our company is one of the industry's top-ranked.
+      Research papers assignment will be considered by your instructors when determining the final grade at the end of your studies. Hence, you should seek research writing to ensure that you submit a quality college paper that will guarantee good grades in the end. When you search for a research paper writing service cheap, you will realize that our company is one of the industrys top-ranked.
        </p>
        </div>
     </div>
