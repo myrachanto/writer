a1aaccb4e597cf485fc227c67bab98131b08fc1b
myrachanto <myrachanto@gmail.com>
2022-11-17 17:02:21 +0300

    orange

--- components/patches/ordering.js
+++ components/patches/ordering.js
@@ -8,7 +8,7 @@ const Ordering = () => {
                 We have over 205 professional writers and 178 editors ready to help you complete that assignment and attain academic excellence. We have made over 50k customers happy with a customer return rate of 7/10 due to 91% customer satisfaction rate. 
                 </p>
             </div>
-            <div className="flex justify-center items-center my-5"><button className="p-2 bg-white coloring1 rounded-lg w-20 mr-5">Login</button> or <button  className="p-2 bg-white bg-orange-500 rounded-lg w-40 ml-5">Order Now</button></div>
+            <div className="flex justify-center items-center my-5"><button className="p-2 bg-white coloring1 rounded-lg w-20 mr-5">Login</button> or <button  className="p-2  bg-orange-500 rounded-lg w-40 ml-5">Order Now</button></div>
         </div>
         </div>
      );
